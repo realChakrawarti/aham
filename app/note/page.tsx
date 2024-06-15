fad1acaacbd000d8e0d73275c9cf49421f112b6b
realChakrawarti <anupam.chakrawarti@outlook.com>
2024-06-16 00:16:10 +0530

    style: note component & heading on hover

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,15 +1,20 @@
-import NoteCard from "@/components/note-card";
-import { getAllDocs } from "@/services/getDocsByPath";
-import { noteDirectory } from "@/services/paths";
+import { HomeCard } from '@/components/home-card';
+import NoteCard from '@/components/note-card';
+import { getAllDocs } from '@/services/getDocsByPath';
+import { noteDirectory } from '@/services/paths';
 
 export default function NotePage() {
-  const allNotes = getAllDocs(noteDirectory, "note");
+  const allNotes = getAllDocs(noteDirectory, 'note');
 
   return (
     <div className="mx-auto container">
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-12 gap-4">
         {allNotes.map((note) => (
-          <NoteCard key={note.slug} {...note} />
+          <HomeCard
+            className={'col-span-12 md:col-span-4'}
+            key={note.title}
+            {...note}
+          />
         ))}
       </div>
     </div>
